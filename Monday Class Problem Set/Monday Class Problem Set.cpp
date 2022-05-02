58230bb582bfc006559128dace69123e3b684c46
matthewj1561 <77714715+matthewj1561@users.noreply.github.com>
2022-05-02 17:06:35 -0600

    Added normalize

--- Monday Class Problem Set/Monday Class Problem Set.cpp
+++ Monday Class Problem Set/Monday Class Problem Set.cpp
@@ -57,7 +57,9 @@ private:
    {
       double aDegrees = convertToDegrees(aRadians);
 
-      aDegrees = aDegrees % 360.0;
+      aDegrees  = fmod(aDegrees, 360.0);
+      
+      return convertToRadians(aDegrees);
    }
 
 public:
@@ -73,24 +75,52 @@ public:
    
    void setDegrees(double aDegrees) 
    {
-      this->aRadians = convertToRadians(aDegrees);
+      this->aRadians = normalize(convertToRadians(aDegrees));
    }
 
    void setRadians(double aRadians)
    {
-      this->aRadians = aRadians;
+      this->aRadians = normalize(aRadians);
    }
 
    void display() const
    {
       cout.setf(ios::fixed | ios::showpoint);
       cout.precision(1);
-      cout << "Angle in Degrees: " << convertToDegrees(aRadians) << endl;
+      cout << "Angle in Degrees: " << convertToDegrees(aRadians) << endl << endl;
    }
 };
 
 
 int main()
 {
+   Angle angleTester1;
 
+   cout << "Test Case: Degrees | Input: 255.0" << endl;
+   angleTester1.setDegrees(255.0);
+   angleTester1.display();
+
+   cout << "Test Case: Get Methods " << endl;
+   cout << "Current angle in radians value: " << angleTester1.getRadians() << endl;
+   cout << "Current angle in degrees value: " << angleTester1.getDegrees() << endl << endl;
+
+   cout << "Test Case: Negative | Input: -20.0" << endl;
+   angleTester1.setDegrees(-20.0);
+   angleTester1.display();
+
+   cout << "Test Case: Greater than 360 | Input: 720.0" << endl;
+   angleTester1.setDegrees(720.0);
+   angleTester1.display();
+  
+   cout << "Test Case: Radians | Input: pi" << endl;
+   angleTester1.setRadians(M_PI);
+   angleTester1.display();
+
+   cout << "Test Case: Radians Greater than 2pi | Input: 3pi" << endl;
+   angleTester1.setRadians(3 * M_PI);
+   angleTester1.display();
+
+   cout << "Test Case: Negative Radians | Input: -4pi" << endl;
+   angleTester1.setRadians(-4 * M_PI);
+   angleTester1.display();
 }
